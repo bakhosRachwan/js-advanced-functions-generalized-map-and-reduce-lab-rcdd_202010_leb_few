a646a2b11ab4cdfbb437bbb8b9634f3ee09abba7
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2020-12-08 22:23:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your functions here
+function map(arr, fun) {
+
+  let s = arr.map(elemet => 
+    (fun(elemet))
+  )
+  return s
+}
+
+
+function reduce(src, cb, startpoint){
+  
+    return src.reduce((acc, val) => {cb}(acc, val)+startpoint})
+    
+
+}  
